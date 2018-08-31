d50dd59f4321ad4bc6261cdd17924d48d78105f2
guskarls <gustav.1996.k@hotmail.com>
2018-08-30 21:43:10 -0400

    Update Stock.py
    Momentumfunktion: ej färdig elr testad

--- Stock.py
+++ Stock.py
@@ -14,4 +14,15 @@ class Stock:
         self.name = name
         print("hej")
 
-        self.data = pdr.get_data_yahoo(start=self.start, end=self.end, tickers=self.name, actions=true)
+        self.data = pdr.get_data_yahoo(start=self.start, end=self.end, tickers=self.name, actions = True)
+        
+    def momentum(self, months): #Ger lista/array med momentum för en aktie i månadsintervall
+        close = []
+        day = 0
+        for i in range(0, (self.end-self.start)/months-1): #Fixa detta så att end & start omvandlas till månader elr använd while loop
+            close.append(self.data.iat[day, df.columns.get_loc('Close')])
+            day = day + months*30
+        momentum = []
+        for i in range(1, len(close)-1): #Blir detta rätt med index elr ska det bara vara len(close)
+            monentum.append(((close[i]-close[i-1])/close[i-1])*100) #tänk på att första värdet inte får ngt momentum, ger det i procentutveckling
+        
